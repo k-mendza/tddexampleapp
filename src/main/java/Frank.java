f30efaac3022f9534f3405c6a85a0866395f4aa5
Karol Mendza <karolmendza@gmail.com>
2019-08-20 16:02:47 +0200

    Added Frank class and Frank tests

--- src/main/java/Frank.java
+++ src/main/java/Frank.java
@@ -1,22 +1,23 @@
 import java.util.Objects;
 
-public class Dollar {
+public class Frank {
+
     private int amount;
 
-    public Dollar(int amount) {
+    public Frank(int amount) {
         this.amount = amount;
     }
 
-    public Dollar times(int factor){
-        return new Dollar( amount * factor);
+    public Frank times(int factor) {
+        return new Frank( amount * factor);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Dollar dollar = (Dollar) o;
-        return amount == dollar.amount;
+        Frank frank = (Frank) o;
+        return amount == frank.amount;
     }
 
     @Override
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -6,17 +6,32 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplication() {
+    void testDollarMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+        assertEquals(new Dollar(10), product);
         product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(15), product);
     }
 
     @Test
-    void testEquality() {
+    void testDollarEquality() {
         assertEquals(new Dollar(1), new Dollar(1));
         assertNotEquals(new Dollar(1), new Dollar(2));
+    }
+
+    @Test
+    void testFrankMultiplication() {
+        Frank five = new Frank(5);
+        Frank product = five.times(2);
+        assertEquals(new Frank(10), product);
+        product = five.times(3);
+        assertEquals(new Frank(15), product);
+    }
+
+    @Test
+    void testFrankEquality() {
+        assertEquals(new Frank(1), new Frank(1));
+        assertNotEquals(new Frank(1), new Frank(2));
     }
 }
