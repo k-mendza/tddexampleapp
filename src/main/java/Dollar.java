2d5ef3382fffdb16e017fd75f9ec2f7158c6008b
Karol Mendza <karolmendza@gmail.com>
2019-08-20 16:25:45 +0200

    Added super class Money

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,26 +1,13 @@
 import java.util.Objects;
 
-public class Dollar {
+public class Dollar extends Money {
     private int amount;
 
     public Dollar(int amount) {
-        this.amount = amount;
+        super(amount);
     }
 
-    public Dollar times(int factor){
-        return new Dollar( amount * factor);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Dollar dollar = (Dollar) o;
-        return amount == dollar.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
+    public Dollar times(int factor) {
+        return new Dollar( super.amount * factor);
     }
 }
--- src/main/java/Frank.java
+++ src/main/java/Frank.java
@@ -1,27 +1,12 @@
 import java.util.Objects;
 
-public class Frank {
-
-    private int amount;
+public class Frank extends Money {
 
     public Frank(int amount) {
-        this.amount = amount;
+        super(amount);
     }
 
     public Frank times(int factor) {
-        return new Frank( amount * factor);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Frank frank = (Frank) o;
-        return amount == frank.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
+        return new Frank( super.amount * factor);
     }
 }
